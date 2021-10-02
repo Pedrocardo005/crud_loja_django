cd18402ca89a8ca5f93c817a93a5783c537a1958
Pedro da Conceicao <pedrocardo007@hotmail.com>
2021-10-02 16:23:35 -0300

    Criando models

--- loja/models.py
+++ loja/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Categoria(models.Model):
+    nome = models.TextField(max_length=255)
+
+class Produto(models.Model):
+    nome = models.TextField(max_length=255)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    preco = models.FloatField()
+    fabricante = models.TextField(max_length=255)
+    vendido = models.BooleanField
